73a52d163e5127241355f05053193815dc971bc5
lmuouml <lmuouml@github.com>
2026-02-10 16:27:36 +0800

    修复rm json逻辑未执行的问题

--- reconftw.sh
+++ reconftw.sh
@@ -3299,16 +3299,8 @@ function fuzz() {
 			cat webs/webs.txt webs/webs_uncommon_ports.txt 2>/dev/null | anew -q webs/webs_all.txt
 		fi
 
-		if [[ -s "webs/webs_all.txt" ]]; then
 			if [[ $AXIOM != true ]]; then
-				interlace -tL webs/webs_all.txt -threads ${INTERLACE_THREADS} -c "ffuf ${FFUF_FLAGS} -t ${FFUF_THREADS} -rate ${FFUF_RATELIMIT} -H \"${HEADER}\" -w ${fuzz_wordlist} -maxtime ${FFUF_MAXTIME} -u _target_/FUZZ -o _output_/_cleantarget_.json -of json" -o $dir/.tmp/fuzzing 2>>"$LOGFILE" >/dev/null
-				for sub in $(cat webs/webs_all.txt); do
-					sub_out=$(echo $sub | sed -e 's|^[^/]*//||' -e 's|/.*$||')
-					if [ -s "$dir/.tmp/fuzzing/${sub_out}.json" ]; then
-						cat "$dir/.tmp/fuzzing/${sub_out}.json" | jq -r 'try .results[] | "\(.status) \(.length) \(.url)"' | sort -k1 | anew -q "$dir/fuzzing/${sub_out}.txt"
-						rm -f "$dir/.tmp/fuzzing/${sub_out}.json" # 核心优化：解析完立即删除，节省空间
-					fi
-				done
+				interlace -tL webs/webs_all.txt -threads ${INTERLACE_THREADS} -c "ffuf ${FFUF_FLAGS} -t ${FFUF_THREADS} -rate ${FFUF_RATELIMIT} -H \"${HEADER}\" -w ${fuzz_wordlist} -maxtime ${FFUF_MAXTIME} -u _target_/FUZZ -o _output_/_cleantarget_.json -of json >/dev/null 2>&1 && [ -f _output_/_cleantarget_.json ] && jq -r 'try .results[] | \"\(.status) \(.length) \(.url)\"' _output_/_cleantarget_.json | sort -k1 | anew -q _output_/_cleantarget_.txt; rm -f _output_/_cleantarget_.json" -o $dir/fuzzing 2>>"$LOGFILE" >/dev/null
 				find $dir/fuzzing/ -type f -iname "*.txt" -exec cat {} + 2>>"$LOGFILE" | sort -k1 | anew -q $dir/fuzzing/fuzzing_full.txt
 			else
 				wget -q -O - ${fuzzing_remote_list} >.tmp/fuzzing_remote_list.txt
